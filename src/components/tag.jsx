f9885e5e6efe25aee0f16e773852382904b9cbca
Huy T Tran <tunhuytrn@gmail.com>
2024-06-20 17:59:29 -0700

    completed aside bard

--- src/components/tag.jsx
+++ src/components/tag.jsx
@@ -3,19 +3,27 @@ import React, { Component } from "react";
 class Tag extends Component {
     constructor(props){
         super(props);
-        if(props){
-            
-        }
+        
         this.state = {
-            bgColor: props.bgColor,
+            bgColor: props.bgColor ? props.bgColor : "#000000",
+            textColor: props.textColor ? props.textColor : "#ffffff",
+            borderColor: props.borderColor ? props.borderColor : "#ffffff",
             tagName: props.tagName
         }
     }
     
     render() {
+        console.log(this.state)
         return (
-            <div className="w-fit m-1 border-solid rounded-md shadow-md shadow-black" style={{backgroundColor: this.state.bgColor}}>
-                <span className="text-center text-xs font-bold text-white m-2">{this.state.tagName}</span>
+            <div className="w-fit m-1 border-solid rounded-full shadow-md shadow-black" 
+                style={{
+                        backgroundColor: this.state.bgColor,
+                        border: '1px solid ' + this.state.borderColor
+                    }}>
+                <span className="text-center text-xs font-bold m-2"
+                style={{color: this.state.textColor}}>
+                    {this.state.tagName}
+                </span>
             </div>
         )
     };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,40 +1,41 @@
 import React from "react";
 import Tag from "../../components/tag";
+import Nav from "../../components/nav";
 
 function Home(){
     return(
-        <>
-            <aside className="m-3 pt-5 pb-5 h-4/5 md:w-1/3 lg:w-1/4 border-solid border-2 rounded-lg border-black">
+        <div className="md:flex items-stretch">
+            <aside className="m-2 pt-5 pb-5 h-4/5 md:w-1/3 lg:w-1/4 border-solid border-2 rounded-lg" style={{borderColor: "#EFBF04"}}>
                 <div className="mr-2 ml-2 grid justify-center">
                     <>
                         {/* Avatar and Full Name */}
                         <div className="grid justify-items-center">
                             <img className="h-32 border-solid rounded-3xl" src={require('../../utils/img/AR_Self.jpg')} alt="Avatar"/>
-                            <span className="font-bold text-xl md:text-2xl">Huy Tuan Tran</span>
+                            <span className="my-4 font-bold text-white text-xl md:text-2xl">Huy Tuan Tran</span>
                         </div>
                         {/* Job Titles */}
                         <div className="flex flex-wrap justify-center">
-                            <Tag bgColor="black" tagName="Software Engineer"/>
-                            <Tag bgColor="black" tagName="Ignition HMI Developer"/>
-                            <Tag bgColor="black" tagName="IBM Full Stack Developer"/>
+                            <Tag bgColor="rgb(38 38 38)" textColor="white" borderColor="#EFBF04" tagName="Software Engineer"/>
+                            <Tag bgColor="rgb(38 38 38)" textColor="white" borderColor="#EFBF04" tagName="Ignition HMI Developer"/>
+                            <Tag bgColor="rgb(38 38 38)" textColor="white" borderColor="#EFBF04" tagName="IBM Full Stack Developer"/>
                         </div>
                     </>
-                    <hr className="h-px my-5 mx-3 bg-gray-200 border-0 dark:bg-gray-700"/>
+                    <hr className="h-px my-5 mx-3 bg-amber-300 border-0"/>
                     <>
                         {/* Contact Info */}
-                        <div className="place-self-center">
-                            <a className="flex hover:drop-shadow-lg" href="mailto:tunhuytrn@gmail.com">
+                        <div className="flex flex-wrap justify-around">
+                            <a className="flex rounded-full px-2 hover:drop-shadow-lg hover:ring" href="mailto:tunhuytrn@gmail.com">
                                 <div className="border-solid border-2 border-gray-500 rounded-md m-2 content-center">
                                     <svg className="h-8 w-8 text-red-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
                                     </svg>
                                 </div>
                                 <div className="flex items-center">
-                                    <span className="font-bold">Email</span>
+                                    <span className="font-bold text-white">Email</span>
                                 </div>
                             </a>
                             
-                            <a className="flex hover:drop-shadow-lg" href="https://www.linkedin.com/in/huy-tran-htt5059/" target="_blank" rel="noreferrer noopener">
+                            <a className="flex rounded-full px-2 hover:drop-shadow-lg hover:ring" href="https://www.linkedin.com/in/huy-tran-htt5059/" target="_blank" rel="noreferrer noopener">
                                 <div className="border-solid border-2 border-gray-500 rounded-md m-2 content-center">
                                     <svg className="h-8 w-8 text-blue-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">
                                         <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
@@ -43,28 +44,28 @@ function Home(){
                                     </svg>
                                 </div>
                                 <div className="flex items-center">
-                                    <span className="font-bold">LinkedIn</span>
+                                    <span className="font-bold text-white">LinkedIn</span>
                                 </div>
-                            </a>
+                            </a>                            
 
-                            <a className="flex hover:drop-shadow-lg" href="https://www.linkedin.com/in/huy-tran-htt5059/" target="_blank" rel="noreferrer noopener">
+                            <a className="flex rounded-full px-2 hover:drop-shadow-lg hover:ring" href="https://www.linkedin.com/in/huy-tran-htt5059/" target="_blank" rel="noreferrer noopener">
                                 <div className="border-solid border-2 border-gray-500 rounded-md m-2 content-center">
-                                    <svg className="h-8 w-8"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">
+                                    <svg className="h-8 w-8 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">
                                         <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
                                     </svg>
                                 </div>
                                 <div className="flex items-center">
-                                    <span className="font-bold">GitHub</span>
+                                    <span className="font-bold text-white">GitHub</span>
                                 </div>
                             </a>
                         </div>
                     </>
                 </div>
             </aside>
-            <div className="m-3 md:w-2/3 lg:w-3/4 md:float-right border-solid border-2 rounded-lg border-black">
-                asd
+            <div className="m-2 md:w-2/3 lg:w-3/4 md:float-right border-solid border-2 rounded-lg border-white">
+                <Nav/>
             </div>
-        </>
+        </div>
     )
 }
 
