85a5ac29788a80ee75913955f43de85d141c7dcd
Huy T Tran <tunhuytrn@gmail.com>
2024-06-29 00:58:03 -0400

    build

--- frontend/src/components/template/contact/Contact.jsx
+++ frontend/src/components/template/contact/Contact.jsx
@@ -3,7 +3,7 @@ import ServiceDialog from "../../Dialog";
 
 
 function Contact(){
-    const [email, setEmail] = useState('');
+    const [recipientEmail, setRecipientEmailEmail] = useState('');
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
     const [isOpen, setOpen] = useState(false);
@@ -42,7 +42,7 @@ function Contact(){
                 <iframe className="w-full h-auto" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1789025.1753237504!2d-81.65895074868769!3d33.431475338956965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f8bafaf828004f%3A0xee8ffc1178a3c6b8!2s21%20Oaks!5e0!3m2!1sen!2sus!4v1719254242226!5m2!1sen!2sus" width="600" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"/>
                 <div className="h-full flex flex-col items-scretch">
                     <form className="w-full h-full" onSubmit={submitHandler}>
-                        <input onChange={(e) => setEmail(e.target.value)} name="email" type="email" className="w-full my-2 p-2 ps-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required/>
+                        <input onChange={(e) => setRecipientEmailEmail(e.target.value)} name="email" type="email" className="w-full my-2 p-2 ps-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required/>
                         <input onChange={(e) => setSubject(e.target.value)} name="subject" type="text" className="w-full my-2 p-2 ps-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Subject" required/>
                         <textarea onChange={(e) => setMessage(e.target.value)} name="body" className="w-full h-full my-2 p-2 ps-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Message" required/>
                         <div className="flex justify-center">
